544a0f638f64bff41f41e40b853efe7793aba4cc
ik512 <ik512@rambler.ru>
2019-10-07 16:35:24 +0300

    les6added

---
+++ WebStore/ViewModels/LoginViewModel - Copy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required,MaxLength(255)]
+        public string UserName { get; set; }
+       
+        [Required,DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Запомнить меня")]
+       public bool RememberMe { get; set; }
+        public string RetunUrl { get; set; }
+    }
+}
